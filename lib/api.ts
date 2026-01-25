94159e1de6e836f9178fcb47a8864cde2227a58f
youn <tty7797@naver.com>
2026-01-25 16:38:20 +0900

    refactor : 서버 주소로 변경

--- lib/api.ts
+++ lib/api.ts
@@ -30,7 +30,7 @@ export interface FilterParams {
   isActive?: boolean
 }
 
-const API_BASE_URL = "http://localhost:8080/api"
+const API_BASE_URL = "http://18.118.126.79:8080/api"
 
 // 스터디 목록 조회 API
 export async function fetchPosts(params: FilterParams = {}): Promise<FilterPostResponse> {
