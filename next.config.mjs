66ae55c0b5c6b04554e813ac66734514f6d4155c
youn <tty7797@naver.com>
2026-02-04 15:12:28 +0900

    fix : 프록시 설정

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,14 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://13.124.208.236:8080/api/:path*',
+      },
+    ]
+  },
 }
 
 export default nextConfig
